fe8c8789a726bf3f40d691c57e083f2c1630ee1e
denialtorres <denial.torres@gmail.com>
2024-06-04 19:23:29 -0600

    Active Record fields

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -5,6 +5,11 @@ class Types::AuthorType < Types::BaseObject
     field :first_name, String, null: false #, camelize: false
     field :last_name, String, null: false
     field :yob, Int, null: false
-    field :is_alive, Boolean, null: false
+    field :is_alive, Boolean, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :full_name, String, null: true
+
+    def full_name
+        "#{object.first_name} #{object.last_name}"
+    end
 end
