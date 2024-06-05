2d8dbf0b496e8f98fc0faf6811517ec006f1f126
denialtorres <denial.torres@gmail.com>
2024-06-04 19:17:39 -0600

    scalar field type

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -6,4 +6,5 @@ class Types::AuthorType < Types::BaseObject
     field :last_name, String, null: false
     field :yob, Int, null: false
     field :is_alive, Boolean, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
 end
